fe33b6d06ebd32124a0304ad3082a364bf6bd8f7
Phornee <phornee@gmail.com>
2020-11-27 12:51:30 +0100

    Reorganize

--- tempsensor.py
+++ tempsensor.py
@@ -4,12 +4,16 @@ import board
 import mariadb
 import sys
 
+
+# --------- User Settings ---------
+SENSOR_LOCATION_NAME = "Buhardilla"
+# ---------------------------------
+
 def getDBConn():
+    """"""
     # Connect to MariaDB Platform
     try:
         conn = mariadb.connect(
-            #user="root",
-            #password="4M4!MukXKlYy",
             user="pi",
             password="i6B#Z*5Afvre",
             host="192.168.0.3",
@@ -23,33 +27,36 @@ def getDBConn():
 
     return conn
 
+def sensorRead(conn, pin):
+    """"""
+    dhtSensor = adafruit_dht.DHT22(pin)
 
-# --------- User Settings ---------
-SENSOR_LOCATION_NAME = "Buhardilla"
-# ---------------------------------
+    try:
+        humidity = dhtSensor.humidity
+        temp_c = dhtSensor.temperature
+
+        if temp_c:
+            # print(SENSOR_LOCATION_NAME + " Temperature(C) {}".format(temp_c))
+            sql = "INSERT INTO hometelemetry.measurements VALUES (null, 0, now(), {})".format(temp_c)
+            cursor.execute(sql)
+
+        if humidity:
+            # print(SENSOR_LOCATION_NAME + " Humidity(%) {}".format(humidity,".2f"))
+            sql = "INSERT INTO hometelemetry.measurements VALUES (null, 1, now(), {})".format(humidity)
+            cursor.execute(sql)
+
+        conn.commit()
+
+    except Exception as e:
+        print("RuntimeError: {}".format(e))
+
 
 conn = getDBConn()
 cursor = conn.cursor()
 
-dhtSensor = adafruit_dht.DHT22(board.D2)
+sensorRead(conn, board.D2)
 
-try:
-    humidity = dhtSensor.humidity
-    temp_c = dhtSensor.temperature
 
-    if temp_c:
-        #print(SENSOR_LOCATION_NAME + " Temperature(C) {}".format(temp_c))
-        sql = "INSERT INTO hometelemetry.measurements VALUES (null, 0, now(), {})".format(temp_c)
-        cursor.execute(sql)
 
-    if humidity:
-        #print(SENSOR_LOCATION_NAME + " Humidity(%) {}".format(humidity,".2f"))
-        sql = "INSERT INTO hometelemetry.measurements VALUES (null, 1, now(), {})".format(humidity)
-        cursor.execute(sql)
-
-    conn.commit()
-
-except Exception as e:
-    print("RuntimeError: {}".format(e))
 
 
