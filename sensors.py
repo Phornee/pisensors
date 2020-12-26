e725408d44d734570c6bd9759bce69737bb4b36e
Phornee <phornee@gmail.com>
2020-12-26 12:22:53 +0100

    Managed class

---
+++ sensors.py
@@ -0,0 +1,45 @@
+import adafruit_dht
+from DBConn import homeTelemetryDB, DBOpenException
+from ManagedClass import ManagedClass
+
+class Sensors(ManagedClass):
+
+    def __init__(self):
+        super().__init__(classname='sensors', execpath=__file__)
+        self.homeDB = homeTelemetryDB()
+
+    def sensorRead(self):
+        """"""
+        try:
+            conn = self.homeDB.openConn()
+            cursor = conn.cursor()
+
+            dhtSensor = adafruit_dht.DHT22(self.config['pin'])
+
+            humidity = dhtSensor.humidity
+            temp_c = dhtSensor.temperature
+
+            if temp_c:
+                # print(SENSOR_LOCATION_NAME + " Temperature(C) {}".format(temp_c))
+                sql = "INSERT INTO hometelemetry.measurements VALUES (null, 0, UTC_TIMESTAMP(), {})".format(self.config['temp_id'], temp_c)
+                cursor.execute(sql)
+
+            if humidity:
+                # print(SENSOR_LOCATION_NAME + " Humidity(%) {}".format(humidity,".2f"))
+                sql = "INSERT INTO hometelemetry.measurements VALUES (null, 1, UTC_TIMESTAMP(), {})".format(self.config['humid_id'], humidity)
+                cursor.execute(sql)
+
+            conn.commit()
+
+        except Exception as e:
+            self.logger.error("RuntimeError: {}".format(e))
+
+
+if __name__ == "__main__":
+    sensors_instance = Sensors()
+    sensors_instance.sensorRead()
+
+
+
+
+
