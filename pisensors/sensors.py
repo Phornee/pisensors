6789f65727f7e25e1b4feef7b487ff0bf178ab23
Phornee <phornee@gmail.com>
2021-03-01 15:14:47 +0100

    change in interface of db

--- pisensors/sensors.py
+++ pisensors/sensors.py
@@ -1,37 +1,40 @@
 import adafruit_dht
-from dbutils_phornee import homeTelemetryDB, DBOpenException
+from dbutils_phornee import MariaDBConn
 from baseutils_phornee import ManagedClass
+from datetime import datetime
 
 class Sensors(ManagedClass):
 
     def __init__(self):
-        super().__init__(classname='sensors', execpath=__file__)
-        self.homeDB = homeTelemetryDB()
+        super().__init__(execpath=__file__)
+        self.homeDB = MariaDBConn()
+
+    @classmethod
+    def getClassName(cls):
+        return "sensors"
 
     def sensorRead(self):
         """
         Read sensors information
         """
         try:
-            conn = self.homeDB.openConn()
-            cursor = conn.cursor()
-
+            self.homeDB.openConn(self.config['mariadbconn'])
             dhtSensor = adafruit_dht.DHT22(self.config['pin'])
 
             humidity = dhtSensor.humidity
             temp_c = dhtSensor.temperature
 
+            timestamp = datetime.utcnow()
+
             if temp_c:
+                data = {"sensor_id": self.config['temp_id'], "timestamp": timestamp, "value": temp_c}
+                self.homeDB.insert('measurements', data)
                 # print(SENSOR_LOCATION_NAME + " Temperature(C) {}".format(temp_c))
-                sql = "INSERT INTO hometelemetry.measurements VALUES (null, {}, UTC_TIMESTAMP(), {})".format(self.config['temp_id'], temp_c)
-                cursor.execute(sql)
 
             if humidity:
+                data = {"sensor_id": self.config['humid_id'], "timestamp": timestamp, "value": humidity}
+                self.homeDB.insert('measurements', data)
                 # print(SENSOR_LOCATION_NAME + " Humidity(%) {}".format(humidity,".2f"))
-                sql = "INSERT INTO hometelemetry.measurements VALUES (null, {}, UTC_TIMESTAMP(), {})".format(self.config['humid_id'], humidity)
-                cursor.execute(sql)
-
-            conn.commit()
 
         except Exception as e:
             self.logger.error("RuntimeError: {}".format(e))
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pisensors",
-    version="0.0.4",
+    version="0.1.0",
     author="Ismael Raya",
     author_email="phornee@gmail.com",
     description="Raspberry Pi Sensors script for Temperature & Humidity",
@@ -23,8 +23,8 @@ setuptools.setup(
         "Topic :: Home Automation"
     ],
     install_requires=[
-        'baseutils_phornee>=0.0.3',
-        'dbutils_phornee>=0.0.3',
+        'baseutils_phornee>=0.0.5',
+        'dbutils_phornee>=0.0.4',
         'adafruit-circuitpython-dht>=3.5.1'
     ],
     python_requires='>=3.6',
