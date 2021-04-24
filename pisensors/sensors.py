4bb43a5b17b042e176f715e1c07234cb3e2a2bed
Phornee <phornee@gmail.com>
2021-04-24 23:26:54 +0200

    Raspberry platform check

--- pisensors/sensors.py
+++ pisensors/sensors.py
@@ -1,4 +1,3 @@
-import adafruit_dht
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from baseutils_phornee import ManagedClass
@@ -23,27 +22,29 @@ class Sensors(ManagedClass):
         """
         Read sensors information
         """
-        try:
-            dhtSensor = adafruit_dht.DHT22(self.config['pin'])
+        if self.is_raspberry_pi():
+            try:
+                import adafruit_dht
+                dhtSensor = adafruit_dht.DHT22(self.config['pin'])
 
-            humidity = dhtSensor.humidity
-            temp_c = dhtSensor.temperature
+                humidity = dhtSensor.humidity
+                temp_c = dhtSensor.temperature
 
-            write_api = self.conn.write_api(write_options=SYNCHRONOUS)
+                write_api = self.conn.write_api(write_options=SYNCHRONOUS)
 
-            point = Point('DHT22') \
-                .tag('sensorid', self.config['id']) \
-                .field('temp', temp_c) \
-                .field('humidity', humidity) \
-                .time(datetime.utcnow(), WritePrecision.NS)
+                point = Point('DHT22') \
+                    .tag('sensorid', self.config['id']) \
+                    .field('temp', temp_c) \
+                    .field('humidity', humidity) \
+                    .time(datetime.utcnow(), WritePrecision.NS)
 
-            write_api.write(self.bucket, self.org, point)
-            # print("Temperature(C) {}".format(temp_c))
-            # print("Humidity(%) {}".format(humidity,".2f"))
+                write_api.write(self.bucket, self.org, point)
+                self.logger.error("Temp: {} | Humid: {}".format(temp_c, humidity))
+                # print("Temperature(C) {}".format(temp_c))
+                # print("Humidity(%) {}".format(humidity,".2f"))
 
-        except Exception as e:
-            self.logger.error("RuntimeError: {}".format(e))
-
+            except Exception as e:
+                self.logger.error("RuntimeError: {}".format(e))
 
 if __name__ == "__main__":
     sensors_instance = Sensors()
