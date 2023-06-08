c3127505d0d39ac531b063506180c0aa8296d35f
Phornee <phornee@gmail.com>
2023-06-08 20:21:41 +0200

    fix version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pisensors",
-    version="0.4.3",
+    version="0.4.4",
     author="Ismael Raya",
     author_email="phornee@gmail.com",
     description="Raspberry Pi Sensors script for Temperature & Humidity",
